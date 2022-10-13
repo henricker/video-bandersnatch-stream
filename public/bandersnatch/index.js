acbab9275e2abb92d7ef994ef5a790be4a500039
henricker <henriquevieira@alu.ufc.br>
2022-10-13 18:17:49 -0300

    chore: (style) add prettier and eslint configs

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -1,12 +1,12 @@
 async function main() {
-    const player = videojs('vid');
-    const ModalDialog = videojs.getComponent('ModalDialog');
-    const modal = new ModalDialog(player, {
-        temporary: false, 
-        closeable: true
-    });
+  const player = videojs("vid");
+  const ModalDialog = videojs.getComponent("ModalDialog");
+  const modal = new ModalDialog(player, {
+    temporary: false,
+    closeable: true,
+  });
 
-    player.addChild(modal);
+  player.addChild(modal);
 }
 
-window.onload = main
+window.onload = main;
