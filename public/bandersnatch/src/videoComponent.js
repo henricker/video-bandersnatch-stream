81a6cc4e904843d681c46fd41aae44abf472922d
henricker <henriquevieira@alu.ufc.br>
2022-10-14 18:40:26 -0300

    feat: get video by media source

---
+++ public/bandersnatch/src/videoComponent.js
@@ -0,0 +1,11 @@
+class VideoComponent {
+  initializePlayer() {
+    const player = videojs("vid");
+    const ModalDialog = videojs.getComponent("ModalDialog");
+    const modal = new ModalDialog(player, {
+      temporary: false,
+      closeable: true,
+    });
+    player.addChild(modal);
+  }
+}
